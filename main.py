8e84904e7023a49f3d88a07b21548e3dc73e763a
Krzysztof Smykla <krzysztof.smykla.agh@gmail.com>
2026-02-26 11:42:06 +0100

    feat: implements a data_scraper script.
    The data scraper downloads all historical data about S&P 500 companies from yfinance. The script uses using Wikipedia to source ticker codes.
    
    The commit fixes "HTTP Error 403: Forbidden" by downloading data from yfinance in batches to bypass rate limits.
    
    Company data is saved to separate .csv files stored in the /data directory.

--- main.py
+++ main.py
@@ -3,28 +3,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import tkinter as tk
 import os
+import time
 
 # set the working directory to the location of the script
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
-
-# Sample dataset
-data = pd.read_csv('stock_price.csv')
-
-print(data.head())
-data['date'] = pd.to_datetime(data['date'])
-data.set_index('date', inplace=True)
-
-aal_data = data[data['Name'] == 'AAL']
-aal_data['close'].plot(figsize=(12, 6))
-plt.title('AAL Stock Price Over Time')
-plt.xlabel('Date')
-plt.ylabel('Price')
-plt.show()
-
-print(data)
-
-
-
 
 # TODO: Implement the following APIs:
 # Stock Price API (https://finnhub.io/docs/api/websocket-trades):
@@ -35,8 +17,28 @@ print(data)
 # Provides access to historical stock price data for companies in every major exchange around the world.
 # Historical price data is available for up to 20 years.
 
+# Collect historical training data from yfinance API
+import yfinance as yf
+# Fetching Microsoft ticker from yfinance 
+msft = yf.Ticker('MSFT')
+msft.info
 
+print("Microsoft Info ", msft.info)
+print("Microsoft Income statement ",pd.DataFrame(msft.income_stmt))
 
+# A test visualization
+msft.history(period='1mo').plot(y='Close', use_index=True, title='Microsoft Stock Price')
+plt.show()
 
+# Fetch historical stock price data for the S&P 500 index
+sp500 = yf.Ticker("^GSPC")
+sp500_history = sp500.history(period='3mo')
+# Visualize the historical data for the S&P 500 index
+sp500_history.plot(y='Close', use_index=True, title='S&P 500 Stock Price')
+plt.show()
 
+# Importing all S&P 500 tickers from Wikipedia using pandas. 
+# This way we can get the most up-to-date list of S&P 500 constituents 
+# without relying on an API that may have limitations or require authentication. 
+# The Wikipedia page is regularly updated and provides a comprehensive list of the companies included in the S&P 500 index.
 
